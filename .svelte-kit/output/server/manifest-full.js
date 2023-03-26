d036fd572107100b71696f389e4cc8383aae6876
Chris Trost <chris@lifeistoasty.com>
2023-03-26 16:47:38 +0200

    klasklsa

---
+++ .svelte-kit/output/server/manifest-full.js
@@ -0,0 +1,27 @@
+export const manifest = {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set([".nojekyll","favicon.png","robots.txt"]),
+	mimeTypes: {".png":"image/png",".txt":"text/plain"},
+	_: {
+		client: {"start":{"file":"_app/immutable/entry/start.4b8cdc2c.js","imports":["_app/immutable/entry/start.4b8cdc2c.js","_app/immutable/chunks/index.6eed8bf6.js","_app/immutable/chunks/singletons.bc03173e.js","_app/immutable/chunks/index.fe00b169.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.d30155f9.js","imports":["_app/immutable/entry/app.d30155f9.js","_app/immutable/chunks/index.6eed8bf6.js"],"stylesheets":[],"fonts":[]}},
+		nodes: [
+			() => import('./nodes/0.js'),
+			() => import('./nodes/1.js'),
+			() => import('./nodes/2.js')
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0], errors: [1], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+};
---
+++ .svelte-kit/output/server/nodes/0.js
@@ -0,0 +1,10 @@
+import * as universal from '../entries/pages/_layout.js';
+
+export const index = 0;
+export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
+export const file = '_app/immutable/entry/_layout.svelte.d58f11dc.js';
+export { universal };
+export const universal_id = "src/routes/+layout.js";
+export const imports = ["_app/immutable/entry/_layout.svelte.d58f11dc.js","_app/immutable/chunks/index.6eed8bf6.js","_app/immutable/chunks/stores.35c368bf.js","_app/immutable/chunks/singletons.bc03173e.js","_app/immutable/chunks/index.fe00b169.js","_app/immutable/entry/_layout.js.984db11e.js","_app/immutable/chunks/_layout.da46b06b.js"];
+export const stylesheets = ["_app/immutable/assets/_layout.fa9427ff.css"];
+export const fonts = ["_app/immutable/assets/fira-mono-cyrillic-ext-400-normal.3df7909e.woff2","_app/immutable/assets/fira-mono-all-400-normal.1e3b098c.woff","_app/immutable/assets/fira-mono-cyrillic-400-normal.c7d433fd.woff2","_app/immutable/assets/fira-mono-greek-ext-400-normal.9e2fe623.woff2","_app/immutable/assets/fira-mono-greek-400-normal.a8be01ce.woff2","_app/immutable/assets/fira-mono-latin-ext-400-normal.6bfabd30.woff2","_app/immutable/assets/fira-mono-latin-400-normal.e43b3538.woff2"];
